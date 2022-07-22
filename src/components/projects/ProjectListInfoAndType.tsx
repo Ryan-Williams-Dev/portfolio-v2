c1947bebe908a9fa8ac1c2c1af85d2b564fe371a
Ryan Williams <r.d.williams.ca@gmail.com>
2022-07-22 15:52:43 -0700

    added screenshots for two other projects

--- src/components/projects/ProjectListInfoAndType.tsx
+++ src/components/projects/ProjectListInfoAndType.tsx
@@ -20,12 +20,12 @@ export const projects: ProjectList = [
   },
   {
     title: "Decision Maker",
-    imageLocation: "/images/projects/scheduler/1.JPG",
+    imageLocation: "/images/projects/decision-maker/1.JPG",
     description: "The Decision Maker is an app that allows users to create a question and collect votes from their friends. When the user creates their poll, they are sent an email with links to a voting page and a results page. Every time someone votes on their poll they are notified by email. The results page displays the options in order of the most poplular rankings.",
   },
   {
     title: "Tweeter",
-    imageLocation: "/images/projects/scheduler/1.JPG",
+    imageLocation: "/images/projects/tweeter/1.JPG",
     description: "A simple single page app. Using jQuery, HTML5, AJAX, and CSS3. The purpose of this project was to make a basic Twitter clone, to practice using CSS styling and clint side scripting to make a dynamic single page app, utilising AJAX http requests.",
   },
 ];
